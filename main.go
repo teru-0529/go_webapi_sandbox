204c47185d260f684353ca3f6b7f4b26e96a36f7
teru-0529 <andrea.pirlo.0529@gmail.com>
2022-09-19 13:39:35 +0900

    SECTION-056 ポート番号を変更できるようにする

--- main.go
+++ main.go
@@ -5,22 +5,33 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
 // main
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) < 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %+v", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
 		log.Printf("failed to terminate server: %+v", err)
 	}
 }
 
 // run
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
@@ -30,7 +41,7 @@ func run(ctx context.Context) error {
 	// 別ゴルーチンでHTTPサーバーを起動
 	eg.Go(func() error {
 		// http.ErrServerClosedは、http.Server.ShutDown()の正常終了であり異常ではない。
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
 		}
