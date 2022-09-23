a54d1b4cb61a737d5f0a2174acd762e7a03a422e
teru-0529 <andrea.pirlo.0529@gmail.com>
2022-09-23 12:33:39 +0900

    SECTION-063 ルーティング定義を分離した「NewMux」を定義する

---
+++ mux_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNewMux(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodGet, "/health", nil)
+
+	sut := NewMux()
+	sut.ServeHTTP(w, r)
+	resp := w.Result()
+	t.Cleanup(func() { _ = resp.Body.Close() })
+
+	// ステータスコードのチェック
+	if resp.StatusCode != http.StatusOK {
+		t.Error("want status code 200, but", resp.StatusCode)
+	}
+
+	// レスポンスボディのチェック
+	got, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("fail to read body: %+v", err)
+	}
+	want := `{"status": "ok"}`
+	if string(got) != want {
+		t.Errorf("want %q, but got %q", want, got)
+	}
+}
